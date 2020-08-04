3a4f314450b7fbb7ed028731ba78871eb65f63dc
Omar ABASSI <oabassi@assurone-group.com>
2020-08-04 15:45:34 +0200

    Add Application console for consumer

---
+++ RabbitMq.MassTransit/App.Message.Consumer/Conusmers/OrderCommandConsumer.cs
@@ -0,0 +1,16 @@
+﻿using App.MassTransit.Messages;
+using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace App.Message.Consumer.Conusmers
+{
+    public class OrderCommandConsumer : IConsumer<IOrderCommand>
+    {
+        public async Task Consume(ConsumeContext<IOrderCommand> context)
+        {
+            Console.WriteLine("consumer ....");
+            Console.WriteLine("context", context.Message.CorrelationId +"titre : " + context.Message.Titre);
+        }
+    }
+}
